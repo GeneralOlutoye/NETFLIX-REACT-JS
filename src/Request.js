181fd884180a58b16a66e504ddcc2935631ef1fa
Joel Olutoye <donronald27@gmail.com>
2022-10-02 10:18:47 +0100

    Updates

--- src/Request.js
+++ src/Request.js
@@ -3,8 +3,8 @@ const key = 'eaef215bf7452cf7cdc9b0430dd97fa2'
 const requests = {
     requestsPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-    requestHorror: `https://api.themoviedb.org/3/movie/horror?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+    requestHorror: `https://api.themoviedb.org/3/movie/{horror}/horror?api_key=${key}`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
 }
 
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,35 +1,31 @@
 import axios from 'axios'
 import React from 'react'
 import { useState, useEffect } from 'react'
+import Movie from './Movie'
 
-const Row = ({title, fetchURL}) => {
+const Row = ({ title, fetchURL }) => {
     const [movies, setMovies] = useState([])
 
-    useEffect(()=>{
-        axios.get(fetchURL).then((response)=>{
+    useEffect(() => {
+        axios.get(fetchURL).then((response) => {
             setMovies(response.data.results)
         })
 
     }, [fetchURL])
     console.log(movies);
-  return (
-    <>
-        <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-        <div className='relative flex items-center'>
-            <div id={'slider'}>
-                {movies.map((item, id) => (
-                    <div className='w-[160px] sm:w-[200px] lg:w-[280px] inline-block cursor-pointer relative p-2 '>
-                        <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                        <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80'>
 
-                        </div>
-
-                    </div>
-                ))}
+    return (
+        <>
+            <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+            <div className='relative flex items-center'>
+                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+                    {movies.map((item, id) => (
+                      <Movie key={id} item={item} />
+                    ))}
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Row
