81ad9998b6c413f9cdecd638781968710eba4a24
Joel Olutoye <donronald27@gmail.com>
2022-10-03 13:53:42 +0100

    Updates

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import Movie from './Movie'
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
@@ -14,15 +15,34 @@ const Row = ({ title, fetchURL }) => {
     }, [fetchURL])
     console.log(movies);
 
+    const slideLeft = () => {
+        var slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+
+    const slideRight = () => {
+        var slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
+
     return (
         <>
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-            <div className='relative flex items-center'>
-                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+            <div className='relative flex items-center group'>
+                <MdChevronLeft
+                    className='bg-white rounded-full left-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+                    size={40}
+                    onClick={slideLeft}
+                />
+                <div id={'slider' + rowID} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.map((item, id) => (
-                      <Movie key={id} item={item} />
+                        <Movie key={id} item={item} />
                     ))}
                 </div>
+                <MdChevronRight
+                    onClick={slideRight}
+                    className='bg-white rounded-full right-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+                    size={40} />
             </div>
         </>
     )
